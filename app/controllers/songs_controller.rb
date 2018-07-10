c8f227556f893bac47426a59a1e9a966705fa611
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 02:29:38 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,6 +11,14 @@ class SongsController < ApplicationController
   def show
   end
 
+  def create
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+  end
+
 private
   def set_song!
     @song = Song.find(params[:id])
