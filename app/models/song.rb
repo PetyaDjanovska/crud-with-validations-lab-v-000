4d2b8fd1b2a3a92214ea8690f434e5b0acdd73e4
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 01:39:08 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,15 +1,15 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year]}
   validates :release_year, presence: true,  if: :released?
-  validate :future_release_year
   validates_absence_of :release_year, unless: :released?
+  validates_numericality_of :release_year, less_than_or_equal_to: :current_year
   validates :artist_name, presence: true
 
-  def future_release_year
-    if self.release_year > Date.current.year
-      errors.add(:release_year, "can not be in the future")
-    end
-  end
+  # def future_release_year
+  #   if self.release_year > Date.current.year
+  #     errors.add(:release_year, "can not be in the future")
+end
+  # end
 
 
 end
