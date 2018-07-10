bb11541c0aed196cd578d99ca2a8116a56fe303e
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 01:39:24 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year]}
   validates :release_year, presence: true,  if: :released?
   validates_absence_of :release_year, unless: :released?
-  validates_numericality_of :release_year, less_than_or_equal_to: :current_year
+  validates :release_year, less_than_or_equal_to: :current_year
   validates :artist_name, presence: true
 
   # def future_release_year
